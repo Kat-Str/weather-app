9732f305dc87d39d41bc1bfbdc4a2099badeb410
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-02-20 21:16:17 +0100

    Added weather and geolocation API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//feature 1
+//real time
 
 let now = new Date();
 
@@ -17,45 +17,59 @@ let day = days[now.getDay()];
 let hour = now.getHours();
 let minutes = now.getMinutes();
 
+let formattedMinutes = String(minutes).padStart(2, "0");
+
 let currentTime = document.querySelector("#current-time");
-currentTime.innerHTML = `${day}, ${hour}:${minutes}`;
+currentTime.innerHTML = `${day}, ${hour}:${formattedMinutes}`;
 
-//feature 2
+//city and temperature
 
+let displayedTemperature = document.querySelector("#tepm-display");
 let formCity = document.querySelector("#form-city");
 let searchEngine = document.querySelector("#search-engine");
 let cityName = document.querySelector("#displayed-city");
+let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+let weatherDescription = document.querySelector("#weather-details");
 
-formCity.addEventListener("submit", displayCity);
+function showDescription(response) {
+  weatherDescription.innerHTML = response.data.weather[0].description;
+}
+
+function showTemp(response) {
+  let cTemp = response.data.main.temp;
+  displayedTemperature.innerHTML = Math.round(cTemp);
+  showDescription(response);
+}
 
 function displayCity(event) {
   event.preventDefault();
   if (searchEngine.value !== "") {
     cityName.innerHTML = searchEngine.value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchEngine.value}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemp);
   }
 }
 
-//feature 3
-let celsius = document.querySelector("#celsius");
-let fahrenheit = document.querySelector("#fahrenheit");
-let displayedTemperature = document.querySelector("#tepm-display");
+formCity.addEventListener("submit", displayCity);
 
-let cTemp = 7;
+//current location
+let currentLocationButton = document.querySelector("#current-location-button");
 
-celsius.addEventListener("click", convertToCelsius);
-fahrenheit.addEventListener("click", convertToFahrenheit);
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  displayedTemperature.innerHTML = cTemp;
-}
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  displayedTemperature.innerHTML = fTemp;
+function showLocalWeather(response) {
+  let crTemp = response.data.current.temp;
+  let crDesc = response.data.current.weather[0].description;
+  displayedTemperature.innerHTML = Math.round(crTemp);
+  weatherDescription.innerHTML = crDesc;
 }
 
-function celsiusToFahrenheit(celsius) {
-  let changedTemp = celsius * (9 / 5) + 32;
-  return Math.round(changedTemp);
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  let currentLocationApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}&units=metric`;
+  axios.get(currentLocationApi).then(showLocalWeather);
 }
-let fTemp = celsiusToFahrenheit(cTemp);
+
+currentLocationButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(getPosition);
+});
