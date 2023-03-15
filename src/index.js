edb95d4a4d84e8f5654d7ecc0df3003f1c6bbd88
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-03-15 21:14:34 +0100

    Added weather forecast using API

--- src/index.js
+++ src/index.js
@@ -35,6 +35,48 @@ let humidity = document.querySelector("#humidity");
 let weatherIcon = document.querySelector("#weather-icon");
 let secondSection = document.querySelector("#main-display");
 
+function formatData(time) {
+  let date = new Date(time * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function showForecast(response) {
+  let forecastArray = response.data.daily;
+  let forecastRow = document.querySelector("#forecast-row");
+  let forecastHTML = `<div class="row">`;
+  forecastArray.forEach((forecastDay, index) => {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col next-day">
+              <h5>${formatData(forecastDay.dt)}</h5>
+              <img
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+                alt="Weather emoji" width="50px"
+              />
+              <p class="temperature">
+                <span class="max-day-temp">${Math.round(
+                  forecastDay.temp.max
+                )}°    </span>
+                <span class="min-day-temp">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              </p>
+            </div>`;
+    }
+  });
+  forecastRow.innerHTML = forecastHTML + `</div>`;
+}
+
+function getForecast(coords) {
+  let apiOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiOneCall).then(showForecast);
+}
+
 function showDescription(response) {
   weatherDescription.innerHTML = response.data.weather[0].description;
   windSpeed.innerHTML = `Wind speed: ${response.data.wind.speed} m/s`;
@@ -49,6 +91,8 @@ function showTemp(response) {
   let cTemp = response.data.main.temp;
   displayedTemperature.innerHTML = Math.round(cTemp);
   showDescription(response);
+  //get coords
+  getForecast(response.data.coord);
 }
 
 function displayCity(event) {
